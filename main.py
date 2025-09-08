c08bbe38d61d6dded7b0a847de34930ef858e127
Rhyster42 <rhysespich@gmail.com>
2025-09-07 22:50:04 -0600

    set up basic screen and game loop at 10 FPS

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import pygame
+import sys
+import random
 from constants import *
 
 
 def main():
     print("Hello Rhys!")
-    print(WINDOW_WIDTH)
+    
+    pygame.init()
 
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption("Snake_Game")
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(BLACK)
+
+        pygame.display.flip()
+        clock.tick(10)
+
+    pygame.quit()
+    sys.exit()
 
 
 
