27bb42a36f4e5a67f46171cb803591cf9532d6e5
Rhyster42 <rhysespich@gmail.com>
2025-09-17 12:40:21 -0600

    updates fruit to spawn inside wall

--- fruit.py
+++ fruit.py
@@ -4,13 +4,13 @@ import random
 
 class Fruit:
     def __init__(self):
-        self.x = random.randint(0, GRID_WIDTH)
-        self.y = random.randint(0, GRID_HEIGHT) # Initialize with random position
+        self.x = random.randint(1, GRID_WIDTH - 1)
+        self.y = random.randint(1, GRID_HEIGHT -1) # Initialize with random position
 
     def draw(self, screen):
         rect = pygame.Rect(self.x * GRID_SIZE, self.y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
         pygame.draw.rect(screen, RED, rect)
 
     def new_position(self):
-        self.x = random.randint(0, GRID_WIDTH)
-        self.y = random.randint(0, GRID_HEIGHT)
+        self.x = random.randint(1, GRID_WIDTH - 1)
+        self.y = random.randint(1, GRID_HEIGHT - 1)
