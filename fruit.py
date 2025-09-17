d02bbc45a78255678dc3e96d05af5e2795b386c7
Rhyster42 <rhysespich@gmail.com>
2025-09-17 16:01:26 -0600

    fixed bug with fruit object spawning

--- fruit.py
+++ fruit.py
@@ -4,13 +4,13 @@ import random
 
 class Fruit:
     def __init__(self):
-        self.x = random.randint(1, GRID_WIDTH - 1)
-        self.y = random.randint(1, GRID_HEIGHT -1) # Initialize with random position
+        self.x = random.randint(2, GRID_WIDTH - 2)
+        self.y = random.randint(2, GRID_HEIGHT - 2) # Initialize with random position
 
     def draw(self, screen):
         rect = pygame.Rect(self.x * GRID_SIZE, self.y * GRID_SIZE, GRID_SIZE, GRID_SIZE)
         pygame.draw.rect(screen, RED, rect)
 
     def new_position(self):
-        self.x = random.randint(1, GRID_WIDTH - 1)
-        self.y = random.randint(1, GRID_HEIGHT - 1)
+        self.x = random.randint(2, GRID_WIDTH - 2)
+        self.y = random.randint(2, GRID_HEIGHT - 2)
